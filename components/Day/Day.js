e6fc7dcc50e1255c5b952d850049153ea75830d0
tspeed90 <tspeed90@gmail.com>
2018-11-30 19:59:01 +0000

    handle clicks on days of calendar

--- components/Day/Day.js
+++ components/Day/Day.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Day = props => {
-  return <div>{props.children}</div>;
+  return <div onClick={() => props.toggleDay(props.index)}>{props.index}</div>;
 };
 
 export default Day;
--- components/container/Container.js
+++ components/container/Container.js
@@ -6,20 +6,37 @@ export default class Container extends Component {
     super(props);
 
     this.state = {
-      numberOfDays: 25
+      numberOfDays: 25,
+      flippedDays: [],
+      unflippedDays: []
     };
   }
 
+  toggleDay = id => {
+    const { flippedDays } = this.state;
+    if (flippedDays.indexOf(id) != 1) {
+      flippedDays.push(id);
+    }
+  };
+
   createDays = () => {
+    const { numberOfDays } = this.state;
     let daysList = [];
 
-    for (let i = 0; i < this.state.numberOfDays; i++) {
-      daysList.push(<Day>{i + 1}</Day>);
+    for (let i = 1; i <= numberOfDays; i++) {
+      daysList.push(
+        <Day
+          id={`day${i}`}
+          key={`day${i}`}
+          index={i}
+          toggleDay={this.toggleDay}
+        />
+      );
     }
     return daysList;
   };
 
   render() {
-    return <Fragment>{this.createDays()}</Fragment>;
+    return <div>{this.createDays()}</div>;
   }
 }
