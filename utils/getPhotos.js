75c8a3cfbf987c3e31032977858d762822a7d5df
tspeed90 <tspeed90@gmail.com>
2018-11-30 22:37:32 +0000

    add getPhotos to fetch Pixabay images

---
+++ utils/getPhotos.js
@@ -0,0 +1,14 @@
+import fetchJsonp from 'fetch-jsonp';
+import { pixabayApi } from '../token.js';
+
+const getPhotos = () => {
+  const images = [];
+  return fetchJsonp(
+    `https://pixabay.com/api/?key=${pixabayApi}&q=christmas&editors_choice=true&page=1`
+  )
+    .then(res => res.json())
+    .then(res => res.hits.map(img => images.push(img.largeImageURL)))
+    .then(() => images);
+};
+
+export { getPhotos };
