7f0f746d286b93ccdf8f92bb90a52448f61be8e5
tspeed90 <tspeed90@gmail.com>
2018-12-01 13:08:49 +0000

    increase number of results from pixabay

--- utils/getPhotos.js
+++ utils/getPhotos.js
@@ -4,7 +4,7 @@ import { pixabayApi } from '../token.js';
 const getPhotos = () => {
   const images = [];
   return fetchJsonp(
-    `https://pixabay.com/api/?key=${pixabayApi}&q=christmas&editors_choice=true&page=1`
+    `https://pixabay.com/api/?key=${pixabayApi}&q=christmas&per_page=40&editors_choice=true&page=1`
   )
     .then(res => res.json())
     .then(res => res.hits.map(img => images.push(img.largeImageURL)))
