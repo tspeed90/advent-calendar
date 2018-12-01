74888f74e7f3225bd14711c6d040bed1bbd73e82
tspeed90 <tspeed90@gmail.com>
2018-12-01 23:48:39 +0000

    add transition to day component on click

--- components/Day/Day.js
+++ components/Day/Day.js
@@ -3,10 +3,22 @@ import './Day.css';
 
 const Day = props => {
   return (
-    <div className="day" onClick={() => props.toggleDay(props.index)}>
+    <div
+      className="day"
+      onClick={() => {
+        props.toggleDay(props.index);
+      }}
+    >
       <div className="number">{!props.flipped && props.index}</div>
       <div className="image__container">
-        {props.flipped && <img src={props.image} />}
+        <img
+          src={props.image}
+          className={
+            !props.flipped
+              ? 'image--hidden image--styled'
+              : 'image--shown image--styled'
+          }
+        />
       </div>
     </div>
   );
