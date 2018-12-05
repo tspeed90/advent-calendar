90c73c484d4f24fc284fe9aab36b35a69416f352
tspeed90 <tspeed90@gmail.com>
2018-12-05 23:05:06 +0000

    minor styling changes, change image search query

--- utils/getPhotos.js
+++ utils/getPhotos.js
@@ -2,7 +2,7 @@ import { APP_ACCESS_KEY } from '../token.js';
 
 const getPhotos = () => {
   return fetch(
-    'https://api.unsplash.com/search/photos?page=1&query=christmas&per_page=30',
+    'https://api.unsplash.com/search/photos?page=1&query=winter-christmas&per_page=30',
     {
       headers: {
         Authorization: `Client-ID ${APP_ACCESS_KEY}`
